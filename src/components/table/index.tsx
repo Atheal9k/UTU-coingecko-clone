5198cee290f507e7aeed51e8a2a4df113146c1c3
Victor <v.huang435@gmail.com>
2021-02-02 21:42:46 +1100

    updated list order

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 interface CoinObject {
   id?: string
-  symbol?: string
+  name?: string
   price_change_percentage_24h_in_currency?: number
   current_price?: number
   market_cap?: number
@@ -18,18 +18,18 @@ const Table: React.FC<Props> = ({ data }) => {
       <table>
         <thead>
           <tr>
-            <th>Symbol</th>
-            <th>24H Change</th>
+            <th>Coin</th>
             <th>Price</th>
+            <th>24H Change</th>
             <th>Market Cap</th>
           </tr>
         </thead>
         <tbody>
           {data.map((coin: CoinObject) => (
             <tr key={coin.id}>
-              <td>{coin.symbol}</td>
-              <td>{coin.price_change_percentage_24h_in_currency}</td>
+              <td>{coin.name}</td>
               <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h_in_currency}</td>
               <td>{coin.market_cap}</td>
             </tr>
           ))}
